13613475878f8c8bbde0fe6cf1d0a5e2f17e08ab
Tolkun Omurbekova <tolkun3922@gmail.com>
2024-09-11 11:49:55 +0600

    refactor: card,typography components

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -1,12 +1,17 @@
-import type { ComponentPropsWithoutRef, ElementType } from 'react'
+import type { ComponentPropsWithoutRef, ElementType, ForwardedRef, forwardRef } from 'react'
+
+import { clsx } from 'clsx'
 
 import s from './Card.module.scss'
 
 export type CardProps<T extends ElementType = 'div'> = {
   as?: T
 } & ComponentPropsWithoutRef<T>
-export const Card = <T extends ElementType = 'div'>(props: CardProps) => {
-  const { as: Component = 'div', className, ...rest } = props
+export const Card = forwardRef(
+  <T extends ElementType = 'div'>(props: CardProps, ref: ForwardedRef<T>) => {
+    const { as: Component = 'div', className, ...rest } = props
+    const cn = clsx(s.card, className)
 
-  return <Component className={clsx(s.card, className)} {...rest} />
-}
+    return <Component className={cn} ref={ref} {...rest} />
+  }
+)
