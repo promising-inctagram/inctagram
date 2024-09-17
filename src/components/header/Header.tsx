8bd59451339fbd1e766f7547bc1e675606ba1183
Kulishevich <maxim8992012@gmail.com>
2024-09-18 01:13:09 +0300

    feat: added Select in Header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,13 +1,21 @@
+import Link from 'next/link'
+
 import styles from './Header.module.scss'
 
 import { Badge } from '../badge'
 import { Button } from '../button'
-import { BellOutlineIcon } from '../icons'
+import { BellOutlineIcon, FlagRussiaIcon, FlagUnitedKingdomIcon } from '../icons'
+import { OptionsValue, Select } from '../select/Select'
 import { Typography } from '../typography'
 
 export type HeaderProps = {
   isAuth: boolean
 }
+
+const selectItemsWithIcons: OptionsValue[] = [
+  { icon: <FlagRussiaIcon />, value: 'Russia' },
+  { icon: <FlagUnitedKingdomIcon />, value: 'English' },
+]
 
 export const Header = (props: HeaderProps) => {
   const { isAuth = false } = props
@@ -25,14 +33,16 @@ export const Header = (props: HeaderProps) => {
             </Badge>
           </Button>
         )}
-        <div className={styles.select}>English</div>
+        <Select defaultValue={selectItemsWithIcons[0].value} options={selectItemsWithIcons} />
         {!isAuth && (
-          <Button style={{ marginLeft: 36 }} variant={'link'}>
+          <Button style={{ marginLeft: 36, width: 100 }} variant={'link'}>
+            {/* <Link href={'auth/'}>Log in</Link> */}
             Log in
           </Button>
         )}
         {!isAuth && (
-          <Button style={{ marginLeft: 24 }} variant={'primary'}>
+          <Button style={{ marginLeft: 24, padding: '6px 20px', width: 100 }} variant={'primary'}>
+            {/* <Link href={'auth/'}>Sign up</Link> */}
             Sign up
           </Button>
         )}
