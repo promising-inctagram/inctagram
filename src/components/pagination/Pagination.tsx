6c556c4b687fda7460312d2624517b7679b6aea4
Kulishevich <maxim8992012@gmail.com>
2024-09-20 02:43:12 +0300

    feat: add pagination functionality

--- src/components/pagination/Pagination.tsx
+++ src/components/pagination/Pagination.tsx
@@ -1,17 +1,104 @@
-import React from 'react'
-import styles from './Pagination.module.scss'
+import React, { useState } from 'react'
+
 import Link from 'next/link'
 
+import styles from './Pagination.module.scss'
+
+import { Button } from '../button'
+import { ArrowIosBackIcon, ArrowIosForwardIcon, MoreHorizontalIcon } from '../icons'
+import { OptionsValue, Select } from '../select/Select'
+
 export type PaginationProps = {
-  countElems: number
+  activePage: number
+  pages: number
+  setActivePage: (current: number) => void
 }
 
+const selectItems: OptionsValue[] = [
+  { value: '10' },
+  { value: '20' },
+  { value: '30' },
+  { value: '50' },
+  { value: '100' },
+]
+
 export const Pagination = (props: PaginationProps) => {
-  const { countElems } = props
-  
+  // const { pages = 8, activePage, setActivePage, selectItems } = props
+  const [activePage, setActivePage] = useState(1)
+  const pages = 55
+
+  //функционал пагинации
+  const getPageNumbers = () => {
+    const pageNumbers: number[] = []
+
+    if (pages < 8) {
+      for (let i = 1; i <= pages; i++) {
+        pageNumbers.push(i)
+      }
+    } else if (activePage < 5) {
+      for (let i = 1; i <= 5; i++) {
+        pageNumbers.push(i)
+      }
+      pageNumbers.push(0) //этот 0 заменяется на Многоточие
+      pageNumbers.push(pages)
+    } else if (activePage > pages - 4) {
+      pageNumbers.push(1)
+      pageNumbers.push(0) //этот 0 заменяется на Многоточие
+      for (let i = pages - 4; i <= pages; i++) {
+        pageNumbers.push(i)
+      }
+    } else {
+      pageNumbers.push(1)
+      pageNumbers.push(0) //этот 0 заменяется на Многоточие
+      for (let i = activePage - 1; i <= activePage + 1; i++) {
+        pageNumbers.push(i)
+      }
+      pageNumbers.push(0) //этот 0 заменяется на Многоточие
+      pageNumbers.push(pages)
+    }
+
+    return pageNumbers
+  }
+
+  const pageNumbersArr = getPageNumbers()
+
   return (
-  <div className={styles.wrapper}>
-    <Link href='d'>1</Link>
-  </div>
+    <div className={styles.wrapper}>
+      <Button className={styles.iconArrow} disabled={activePage === 1} variant={'icon'}>
+        <ArrowIosBackIcon onClick={() => setActivePage(activePage - 1)} />
+      </Button>
+      {pageNumbersArr.map((elem, index) =>
+        elem !== 0 ? (
+          <button
+            className={`${styles.elemPage} ${activePage === elem && styles.activePage}`}
+            key={index}
+            onClick={() => setActivePage(elem)}
+            type={'button'}
+          >
+            {elem}
+          </button>
+        ) : (
+          <MoreHorizontalIcon className={styles.icon} key={index} />
+        )
+      )}
+      <Button
+        className={styles.iconArrow}
+        disabled={activePage === pages}
+        style={{ marginLeft: '12px' }}
+        variant={'icon'}
+      >
+        <ArrowIosForwardIcon onClick={() => setActivePage(activePage + 1)} />
+      </Button>
+
+      <div className={styles.container}>
+        <span style={{ marginLeft: '24px', marginRight: '4px' }}>Show</span>
+        <Select
+          className={styles.select}
+          defaultValue={selectItems[0].value}
+          options={selectItems}
+        />
+        <span style={{ marginLeft: '7px' }}>on page</span>
+      </div>
+    </div>
   )
 }
