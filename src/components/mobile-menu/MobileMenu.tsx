4f66aa59571e57bb5bbaeeec06e21e4c0fa1dc85
Kristina-Tur <kristina.tur06121955@gmail.com>
2024-09-24 15:17:39 +0300

    init: start MobileMenu component

---
+++ src/components/mobile-menu/MobileMenu.tsx
@@ -0,0 +1,13 @@
+import { Button } from '@/components/button'
+import { menuItems } from '@/components/sidebar/menu-items'
+import Link from 'next/link'
+
+export const MobileMenu = () => {
+  return (
+    <div>
+      {/*{menuItems.map((path, i) => (
+        <Button as={Link} href={path} key={i} />
+      ))}*/}
+    </div>
+  )
+}
