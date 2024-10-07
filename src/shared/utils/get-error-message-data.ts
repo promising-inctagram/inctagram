6e1451c36b849adac19afb9c0a06033a5144c31e
Ivan Dolgikh <ivan.dolgikh1199@gmail.com>
2024-10-07 18:31:40 +0300

    fix: get-error-message util

--- src/shared/utils/get-error-message-data.ts
+++ src/shared/utils/get-error-message-data.ts
@@ -3,7 +3,7 @@ import { FetchBaseQueryError } from '@reduxjs/toolkit/query'
 export type FormErrorData = { field: string; message: string }
 
 export type ServerErrorData = {
-  message: FormErrorData[]
+  errorsMessages: FormErrorData[]
   path: string
   timestamp: string
 }
@@ -18,8 +18,8 @@ export function getErrorMessageData(error: unknown) {
     if ('data' in error) {
       const errorData = error as CustomerError
 
-      if ('message' in errorData.data) {
-        return errorData.data.message
+      if ('errorsMessages' in errorData.data) {
+        return errorData.data.errorsMessages
       }
     }
   } else if (isErrorWithMessage(error)) {
