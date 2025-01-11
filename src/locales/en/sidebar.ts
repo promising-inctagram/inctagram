d6af04d369169642dabec03be642f933587ed0b1
atpradical <ivan.dolgikh1199@gmail.com>
2025-01-11 12:54:43 +0300

    Remove pnpm-lock.yaml file
    The pnpm-lock.yaml file was deleted, potentially to ensure compatibility with updates or changes in the package manager or dependencies. This may also signify a shift in the project's dependency management strategy. Ensure to verify dependency resolution and package consistency.

--- src/locales/en/sidebar.ts
+++ src/locales/en/sidebar.ts
@@ -2,7 +2,7 @@ export const sidebar = {
   create: 'Create',
   favourites: 'Favourites',
   home: 'Home',
-  log_out: 'Log out',
+  log_out: 'Log Out',
   messenger: 'Messenger',
   my_profile: 'My profile',
   search: 'Search',
